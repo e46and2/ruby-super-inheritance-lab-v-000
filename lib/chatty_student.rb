73d5e19ee6e71d777d9ec19ef27455a9affdbcad
Emily Nicholl Liepe <e.nicholl.liepe@gmail.com>
2019-02-12 23:26:12 +0000

    Automatically backed up by Learn

---
+++ lib/chatty_student.rb
@@ -0,0 +1,7 @@
+class Chatty_Student < Student 
+  
+  def hello
+    super
+    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was "
+  
+end
